c889d7ff28a4c9d3a16cabaf6dd682612f62d3c7
muhmmad_medhat <muhmmedmedhat0@gmail.com>
2026-02-15 20:51:05 +0200

    Add Pizza component and update app to display pizzeria menu

--- src/app.js
+++ src/app.js
@@ -1,5 +1,23 @@
+const Pizza = (props) => {
+  return React.createElement('div', {}, [
+    React.createElement('h1', {}, 'The Pepperoni Pizza'),
+    React.createElement(
+      'p',
+      {},
+      'The classic pizza with pepperoni, tomato sauce, and mozzarella cheese.',
+    ),
+  ]);
+};
+
 const app = () => {
-  return React.createElement('div', {}, 'Hello World');
+  return React.createElement('div', {}, [
+    React.createElement('h1', {}, "Welcome to Padre Gino's Pizzeria"),
+    React.createElement(Pizza, {}),
+    React.createElement(Pizza, {}),
+    React.createElement(Pizza, {}),
+    React.createElement(Pizza, {}),
+    React.createElement(Pizza, {}),
+  ]);
 };
 
 const container = document.getElementById('root');
