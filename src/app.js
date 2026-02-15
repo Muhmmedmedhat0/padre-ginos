c3bef765e3bd2d6ddb35ec55edb835e41ded05f9
muhmmad_medhat <muhmmedmedhat0@gmail.com>
2026-02-15 20:51:27 +0200

    Make Pizza component dynamic with props and add variety of pizza types

--- src/app.js
+++ src/app.js
@@ -1,22 +1,38 @@
 const Pizza = (props) => {
   return React.createElement('div', {}, [
-    React.createElement('h1', {}, 'The Pepperoni Pizza'),
-    React.createElement(
-      'p',
-      {},
-      'The classic pizza with pepperoni, tomato sauce, and mozzarella cheese.',
-    ),
+    React.createElement('h1', {}, props.name),
+    React.createElement('p', {}, props.description),
   ]);
 };
 
 const app = () => {
   return React.createElement('div', {}, [
     React.createElement('h1', {}, "Welcome to Padre Gino's Pizzeria"),
-    React.createElement(Pizza, {}),
-    React.createElement(Pizza, {}),
-    React.createElement(Pizza, {}),
-    React.createElement(Pizza, {}),
-    React.createElement(Pizza, {}),
+    React.createElement(Pizza, {
+      name: 'The Pepperoni Pizza',
+      description:
+        'The classic pizza with pepperoni, tomato sauce, and mozzarella cheese.',
+    }),
+    React.createElement(Pizza, {
+      name: 'The Hawaiian Pizza',
+      description:
+        'The controversial pizza with ham, pineapple, tomato sauce, and mozzarella cheese.',
+    }),
+    React.createElement(Pizza, {
+      name: 'The Margherita Pizza',
+      description:
+        'The traditional pizza with tomato sauce, fresh mozzarella, and basil.',
+    }),
+    React.createElement(Pizza, {
+      name: 'The Vegetarian Pizza',
+      description:
+        'The healthy pizza with mushrooms, bell peppers, onions, and olives.',
+    }),
+    React.createElement(Pizza, {
+      name: 'The Meat Lovers Pizza',
+      description:
+        'The hearty pizza with pepperoni, sausage, bacon, and ham.',
+    }),
   ]);
 };
 
