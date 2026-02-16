354f2f9af74a13cfd7fccaa950618d5a01fd277e
muhmmad_medhat <muhmmedmedhat0@gmail.com>
2026-02-17 00:06:28 +0200

    feat: update pizza component structure and add styles to index.html

--- src/app.jsx
+++ src/app.jsx
@@ -7,10 +7,12 @@ export default function App() {
       <Pizza
         name="Margherita"
         description="Tomato sauce, mozzarella, and basil"
+        image="/public/pizzas/mediterraneo.webp"
       />
       <Pizza
         name="Pepperoni"
         description="Tomato sauce, mozzarella, and pepperoni"
+        image="/public/pizzas/pepperoni.webp"
       />
     </>
   );
--- src/components/pizza.jsx
+++ src/components/pizza.jsx
@@ -1,8 +1,9 @@
 export function Pizza(props) {
   return (
-    <>
+    <div className="pizza">
       <h2>{props.name}</h2>
       <p>{props.description}</p>
-    </>
+      <img src={props.image} alt={props.name} />
+    </div>
   );
 }
